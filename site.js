f9bfd5224be9410c68e623c5e2b1471334ba3207
corysillars <corysillars@live.com>
2025-10-06 14:09:51 -0500

    added carousel

--- site.js
+++ site.js
@@ -14,3 +14,35 @@ if (isMorning) {
 }
 const key = "It's a secret to everybody"
 localStorage.setItem(key, 'You have found it! You get a gem!!!') // store a secret message in local storage
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+document.querySelector('#next').addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+document.querySelector('#prev').addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
+setInterval(() => {
+    currentImage++
+    showImages()
+},2000) 
